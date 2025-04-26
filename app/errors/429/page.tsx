1852dc4b47122c19d9d6aa28fab2220c0e3d06f4
Hamid6426 <mianhamid6426@gmail.com>
2025-04-26 12:06:19 +0500

    ERROR PAGES

---
+++ app/errors/429/page.tsx
@@ -0,0 +1,14 @@
+import { MdErrorOutline } from "react-icons/md";
+
+// app/400/page.tsx
+export default function TooManyRequest() {
+  return (
+    <div className="absolute top-0 w-full h-screen flex items-center justify-center bg-[#000010]">
+      <div className="text-[#4499ff] w-full py-8 max-w-[240px] border-[#49f] border-2 aspect-video flex flex-col gap-4 justify-center items-center">
+        <MdErrorOutline className="text-5xl " />
+        <h1 className="text-2xl font-bold">Too Many Request!</h1>
+        <h1 className="text-2xl font-bold">Error - 429</h1>
+      </div>
+    </div>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="bg-[#000010] min-h-screen w-full">
-      <div></div>
+      <div className="h-[calc(100vh_-_10rem)]"></div>
     </main>
   );
 }
